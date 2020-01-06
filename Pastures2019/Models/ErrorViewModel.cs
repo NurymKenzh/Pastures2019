66e1f3d76d73006c57e6b62bbf92ff5de1d1f11e
Nurym Kenzh <nak290284@gmail.com>
2020-01-06 13:18:46 +0600

    Add project files.

---
+++ Pastures2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pastures2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

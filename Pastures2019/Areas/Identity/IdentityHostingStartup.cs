a4562cfcbcc8522ceebbf0255ca37094caab32d5
Nurym Kenzh <nak290284@gmail.com>
2020-01-06 14:22:38 +0600

    Identity scaffolding

---
+++ Pastures2019/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pastures2019.Data;
+
+[assembly: HostingStartup(typeof(Pastures2019.Areas.Identity.IdentityHostingStartup))]
+namespace Pastures2019.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

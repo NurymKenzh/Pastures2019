ce86d69ed965b24faa25efdb9ffe0e68068ef1d6
Nurym Kenzh <nak290284@gmail.com>
2020-01-20 16:15:36 +0600

    Modis viewer map

---
+++ Pastures2019/Controllers/MapsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pastures2019.Controllers
+{
+    public class MapsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Modis()
+        {
+            return View();
+        }
+    }
+}

2fa7ab26a51ccca7dd9bb6e4ef7e8268c8142438
Nurym Kenzh <nak290284@gmail.com>
2020-01-27 20:17:51 +0600

    Anomaly show on map

--- Pastures2019/Controllers/MapsController.cs
+++ Pastures2019/Controllers/MapsController.cs
@@ -28,25 +28,51 @@ namespace Pastures2019.Controllers
             ViewBag.GeoServerUrl = Startup.Configuration["GeoServerUrl"].ToString();
             ViewBag.ModisWorkspace = Startup.Configuration["ModisWorkspace"].ToString();
             ViewBag.ModisLayerTemplate1 = Startup.Configuration["ModisLayerTemplate1"].ToString();
-            ViewBag.ModisLayerTemplate2 = Startup.Configuration["ModisLayerTemplate2"].ToString();
-            ViewBag.YearDays = GetModisYearDays();
+            ViewBag.ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI = Startup.Configuration["ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI"].ToString();
+            ViewBag.ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI_Anomaly = Startup.Configuration["ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI_Anomaly"].ToString();
+            ViewBag.YearDays_MOLT_MOD13Q1006_B01_NDVI = GetModisYearDays_MOLT_MOD13Q1006_B01_NDVI();
+            ViewBag.YearDays_MOLT_MOD13Q1006_B01_NDVI_Anomaly = GetModisYearDays_MOLT_MOD13Q1006_B01_NDVI_Anomaly();
             return View();
         }
 
-        private YearDay[] GetModisYearDays()
+        private YearDay[] GetModisYearDays_MOLT_MOD13Q1006_B01_NDVI()
         {
             List<YearDay> yearDays = new List<YearDay>();
             string[] layers = GetModisLayers();
             for (int i = 0; i < layers.Length; i++)
             {
                 layers[i] = layers[i]
-                    .Replace(Startup.Configuration["ModisLayerTemplate1"].ToString(), "")
-                    .Replace(Startup.Configuration["ModisLayerTemplate2"].ToString(), "");
-                yearDays.Add(new YearDay()
+                    .Replace(Startup.Configuration["ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI"].ToString(), "")
+                    .Replace(Startup.Configuration["ModisLayerTemplate1"].ToString(), "");
+                if(layers[i].Length == 7)
                 {
-                    year = layers[i].Substring(0, 4),
-                    day = layers[i].Substring(4, 3)
-                });
+                    yearDays.Add(new YearDay()
+                    {
+                        year = layers[i].Substring(0, 4),
+                        day = layers[i].Substring(4, 3)
+                    });
+                }
+            }
+            return yearDays.ToArray();
+        }
+
+        private YearDay[] GetModisYearDays_MOLT_MOD13Q1006_B01_NDVI_Anomaly()
+        {
+            List<YearDay> yearDays = new List<YearDay>();
+            string[] layers = GetModisLayers();
+            for (int i = 0; i < layers.Length; i++)
+            {
+                layers[i] = layers[i]
+                    .Replace(Startup.Configuration["ModisLayerTemplate_MOLT_MOD13Q1006_B01_NDVI_Anomaly"].ToString(), "")
+                    .Replace(Startup.Configuration["ModisLayerTemplate1"].ToString(), "");
+                if(layers[i].Length == 7)
+                {
+                    yearDays.Add(new YearDay()
+                    {
+                        year = layers[i].Substring(0, 4),
+                        day = layers[i].Substring(4, 3)
+                    });
+                }
             }
             return yearDays.ToArray();
         }
